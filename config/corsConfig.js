c8fbeff825c480232121fa9e23c139be77237ccf
batdimoiprint <johnkennyreyes21@gmail.com>
2025-09-26 23:53:23 +0800

    Added WebView2 localhost domain

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,5 +1,5 @@
 const options = {
-    origin: ['http://localhost:5500'],
+    origin: ['http://localhost:5500', 'https://df0091eeb1e5193e.localhost'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 };
