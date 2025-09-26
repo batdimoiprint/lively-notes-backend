c19e96fa58ed92e882424abae883f3dfb37baae7
batdimoiprint <johnkennyreyes21@gmail.com>
2025-09-26 10:48:34 +0800

    feat: implement CORS configuration

---
+++ config/corsConfig.js
@@ -0,0 +1,6 @@
+const options = {
+    origin: ['http://localhost/'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+};
+module.exports = options
--- index.js
+++ index.js
@@ -2,16 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const cors = require('cors')
+const options = require('./config/corsConfig');
 
 const notesRouter = require('./routes/notesRoutes');
 
-app.use('/api/notes', notesRouter);
 
+app.use(express.json());
+app.use('/api/notes', notesRouter);
+app.use(cors(options))
+
+
+app.get('/', (req, res) => {
+    res.send('Bakit ka andito')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
