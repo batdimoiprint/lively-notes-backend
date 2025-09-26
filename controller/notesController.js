617b426b4d776c946debdfbf2c8b3087dcc46aa0
batdimoiprint <johnkennyreyes21@gmail.com>
2025-09-26 09:54:00 +0800

    fix: commented clgs

--- controller/notesController.js
+++ controller/notesController.js
@@ -1,9 +1,9 @@
-const notesService = require('../service/notesService');
+const notesService = require("../service/notesService");
 
 async function listNotes(req, res, next) {
   try {
-    const notes = await notesService.getAll()
-    console.log(notes)
+    const notes = await notesService.getAll();
+    // console.log(notes)
     res.status(200).json(notes);
   } catch (err) {
     next(err);
@@ -11,15 +11,13 @@ async function listNotes(req, res, next) {
 }
 
 async function createNote(req, res, next) {
-    try {
-        console.log(req.body)
-        const create = await notesService.create(req.body)
-        res.status(201).json(create)
-        
-    } catch (err) {
-        next(err)
-    }
-    
+  try {
+    // console.log(req.body)
+    const create = await notesService.create(req.body);
+    res.status(201).json(create);
+  } catch (err) {
+    next(err);
+  }
 }
 
-module.exports = {listNotes, createNote};
+module.exports = { listNotes, createNote };
