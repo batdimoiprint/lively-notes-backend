950297f1a2ce841c9bda7b0f3d63c1a7ca984e08
batdimoiprint <johnkennyreyes21@gmail.com>
2026-01-01 02:59:54 +0800

    added patch cors

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -5,6 +5,6 @@ const options = {
     "http://localhost:3000",
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 };
 module.exports = options;
